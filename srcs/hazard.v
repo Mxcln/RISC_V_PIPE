50b071d487e19b1a58b865f03889bc0bde0ba54f
WindaYau <windayau233.gmail.com>
2022-12-04 23:44:15 +0800

    修改id与ex逻辑

---
+++ srcs/hazard.v
@@ -0,0 +1,16 @@
+
+`include "define.v"
+
+module  hazard(
+    input   wire                arst_n  ,
+
+    input   wire    ex_
+);
+
+
+
+
+
+    assign  hazard_hold_o = id_ex_mem_r_i & id_ex_
+
+endmodule
